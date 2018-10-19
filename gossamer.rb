e9f5135f5744ed35a2d462e3b1f0e9327e2d8464
Russell Endicott <rendicott@gmail.com>
2018-10-19 09:00:40 -0400

    Update gossamer.rb

--- gossamer.rb
+++ gossamer.rb
@@ -1,8 +1,8 @@
 class Gossamer < Formula
     desc "A command line tool to perform AWS IAM assume-roles"
     homepage "https://github.com/GESkunkworks/gossamer"
-    url "https://github.com/GESkunkworks/gossamer/releases/download/1.2.6/gossamer-darwin-amd64-1.2.6.33.tar.gz"
-    version "1.2.6.33"
+    url "https://github.com/GESkunkworks/gossamer/releases/download/v1.2.2.56/gossamer-darwin-amd64-1.2.2.56.tar.gz"
+    version "1.2.2.56"
   
     def install
       bin.install "gossamer"
