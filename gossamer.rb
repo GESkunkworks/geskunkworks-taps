6a978b3a09f6c8463deab340fddacc5b94e573e6
Russell Endicott <rendicott@gmail.com>
2020-01-13 17:21:20 -0500

    release 2.0.2

--- gossamer.rb
+++ gossamer.rb
@@ -1,10 +1,10 @@
 class Gossamer < Formula
     desc "A command line tool to perform AWS IAM assume-roles using permanent credentials or SAML"
     homepage "https://github.com/GESkunkworks/gossamer"
-    url "https://github.com/GESkunkworks/gossamer/releases/download/2.0.1/gossamer-darwin-amd64-2.0.1.77.tar.gz"
+    url "https://github.com/GESkunkworks/gossamer/releases/download/2.0.2/gossamer-darwin-amd64-2.0.2.90.tar.gz"
     # openssl dgst -sha256 <file>
-    sha256 "62994ae3c1bbe40be6550e25a589647fb5740416aa004655d7099eaf58f924f5"
-    version "2.0.1.77"
+    sha256 "e28da3fc939d59963120238bd4983e9cc107be3ea707d38f6e69e45dc1ceea25"
+    version "2.0.2.90"
   
     def install
       bin.install "gossamer"
