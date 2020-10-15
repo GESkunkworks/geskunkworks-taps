6dad669bf75d5b5397ef425b055593eeae3b589e
Michael Palmer <MichaelPalmer1@users.noreply.github.com>
2020-10-15 17:02:28 -0400

    Update gossamer3.rb

--- gossamer3.rb
+++ gossamer3.rb
@@ -1,18 +1,18 @@
 class Gossamer3 < Formula
     desc "A command line tool to perform AWS IAM assume-roles using permanent credentials or SAML"
     homepage "https://github.com/GESkunkworks/gossamer3"
-    version "3.1.0"
+    version "3.1.1"
     
     on_macos do
         url "https://github.com/GESkunkworks/gossamer3/releases/download/v#{version}/gossamer3_#{version}_darwin_amd64.tar.gz"
         # openssl dgst -sha256 <file>
-        sha256 "af90a65d5c3089747798bcc85240c1b5a5ae1db8d249d1842d0d111673f2359b"
+        sha256 "515f6956faeafcdf8a1ed8bfbe791a2849a8638a305cae41df68c29037253e73"
     end
     
     on_linux do
         url "https://github.com/GESkunkworks/gossamer3/releases/download/v#{version}/gossamer3_#{version}_linux_amd64.tar.gz"
         # openssl dgst -sha256 <file>
-        sha256 "7086b102596a0a91846024b0d56125a7a449e0e4e5adcac4e171418bac5129b3"
+        sha256 "ccbbb322ecb28acac8f396b2b47d4f9ad96cae74d2b1f72cefec957aeb1e9881"
     end
   
     def install
